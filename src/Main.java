0cce6b39332b9c9f3f3d7f5f553f060da9dc94c6
Svetlana <rus_lana@bk.ru>
2023-01-09 11:43:57 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 658_142;
+        int bonus = 20;
+        {
+            int mile;
+            mile = price / bonus;
+            System.out.print("Количество бонусных миль - " + mile);
+        }
+
+
+    }
+}
